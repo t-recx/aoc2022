4404f670097265dd31e6d8d7ec62290f25994eb3
João Bruno <jnnlbruno@gmail.com>
2022-12-01 18:54:16 +0000

    Golfed it a little

--- day1.rb
+++ day1.rb
@@ -1,4 +1,4 @@
-elves_calories = File.readlines('day1.input').map(&:strip).chunk(&:empty?).reject(&:first).map(&:last).map { |x| x.map(&:to_i) }.map(&:sum)
+elves_calories = File.readlines('day1.input').map(&:strip).chunk(&:empty?).reject(&:first).map(&:last).map { |x| x.map(&:to_i).sum }
 
 p elves_calories.max
 
