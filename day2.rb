9ab3522088396397df07a1610a5583eede0772b5
João Bruno <jnnlbruno@gmail.com>
2022-12-02 20:54:57 +0000

    Added day2

---
+++ day2.rb
@@ -0,0 +1,4 @@
+h = { 1 => { 1 => 3, 2 => 0, 3 => 6 }, 2 => { 1 => 6, 2 => 3, 3 => 0 }, 3 => { 1 => 0, 2 => 6, 3 => 3 } }
+a = File.readlines('day2.input').map { |x| x.strip.split.map { |y| { 'A' => 1, 'X' => 1, 'B' => 2, 'Y' => 2, 'C' => 3, 'Z' => 3 }[y] } }
+b = a.map { |x| [x[0], h[x[0]].select { |k, v| v == { 1 => 6, 2 => 3, 3 => 0 }[x[1]] }.first[0]] }
+[a, b].each { |part| p part.map { |x| x[1] + h[x[1]][x[0]] }.sum }
