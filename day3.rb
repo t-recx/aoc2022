ba98fe07b0c9c6a737a3330d3f6250d603a9cfe8
João Bruno <jnnlbruno@gmail.com>
2022-12-03 13:51:08 +0000

    Formatting

--- day3.rb
+++ day3.rb
@@ -1,107 +1,107 @@
 def get_priority(letter)
-	subtraction = nil
-	
-	if letter >= 'a'  
-		subtraction = 96
-	else
-		subtraction = 38
-	end
-	
-	return letter.ord - subtraction
+  subtraction = nil
+
+  if letter >= "a"
+    subtraction = 96
+  else
+    subtraction = 38
+  end
+
+  return letter.ord - subtraction
 end
 
 def part_a(input)
-	sum = 0
-	
-	for i in 0..input.length - 1
-		line = input[i]
-	
-		split_point = (line.length - 1) / 2
-	
-		first_compartment_letters = {}
-		second_compartment_letters = {}
-	
-		common_letter = nil
-	
-		for j in 0..split_point - 1
-			first_char = line[j]
-			second_char = line[split_point + j]
-			
-			first_compartment_letters[first_char] = nil
-			second_compartment_letters[second_char] = nil
-		
-			if first_compartment_letters.keys.include?(second_char)
-				common_letter = second_char
-				break
-			end
-		
-			if second_compartment_letters.keys.include?(first_char)
-				common_letter = first_char
-				break
-			end
-		end
-	
-		sum += get_priority(common_letter)
-	end
-	
-	return sum
+  sum = 0
+
+  for i in 0..input.length - 1
+    line = input[i]
+
+    split_point = (line.length - 1) / 2
+
+    first_compartment_letters = {}
+    second_compartment_letters = {}
+
+    common_letter = nil
+
+    for j in 0..split_point - 1
+      first_char = line[j]
+      second_char = line[split_point + j]
+
+      first_compartment_letters[first_char] = nil
+      second_compartment_letters[second_char] = nil
+
+      if first_compartment_letters.keys.include?(second_char)
+        common_letter = second_char
+        break
+      end
+
+      if second_compartment_letters.keys.include?(first_char)
+        common_letter = first_char
+        break
+      end
+    end
+
+    sum += get_priority(common_letter)
+  end
+
+  return sum
 end
 
 def part_b(input)
-	sum = 0
-	number_elves_per_group = 3
-	
-	for i in 0..((input.length - 1)/number_elves_per_group)
-		j = i * number_elves_per_group
+  sum = 0
+  number_elves_per_group = 3
 
-		elves_rucksacks = input[j..j + number_elves_per_group - 1]
+  for i in 0..((input.length - 1) / number_elves_per_group)
+    j = i * number_elves_per_group
 
-		elves_letters = []
+    elves_rucksacks = input[j..j + number_elves_per_group - 1]
 
-		for k in 0..number_elves_per_group - 1
-			elves_letters[k] = {}
-		end
-	
-		biggest_length = 0
+    elves_letters = []
 
-		for k in 0..number_elves_per_group - 1
-			if elves_rucksacks[k].length > biggest_length
-				biggest_length = elves_rucksacks[k].length			
-			end
-		end
+    for k in 0..number_elves_per_group - 1
+      elves_letters[k] = {}
+    end
 
-		for k in 0..biggest_length - 1
-			for y in 0..number_elves_per_group - 1
-				elves_letters[y][elves_rucksacks[y][k]] = nil
+    biggest_length = 0
 
-				common_letter = nil
+    for k in 0..number_elves_per_group - 1
+      if elves_rucksacks[k].length > biggest_length
+        biggest_length = elves_rucksacks[k].length
+      end
+    end
 
-				for z in 0..number_elves_per_group - 1
-					if elves_letters[z].include?(elves_rucksacks[y][k])
-						common_letter = elves_rucksacks[y][k]
-					else
-						common_letter = nil
-						break
-					end
-				end
+    for k in 0..biggest_length - 1
+      for y in 0..number_elves_per_group - 1
+        elves_letters[y][elves_rucksacks[y][k]] = nil
 
-				if common_letter
-					break
-				end
-			end
+        common_letter = nil
 
-			if common_letter
-				break
-			end
-		end
+        for z in 0..number_elves_per_group - 1
+          if elves_letters[z].include?(elves_rucksacks[y][k])
+            common_letter = elves_rucksacks[y][k]
+          else
+            common_letter = nil
+            break
+          end
+        end
 
-		sum += get_priority(common_letter)
-	end
+        if common_letter
+          break
+        end
+      end
 
-	return sum
+      if common_letter
+        break
+      end
+    end
+
+    sum += get_priority(common_letter)
+  end
+
+  return sum
 end
 
-input = File.readlines('day3.input')
+input = File.readlines("day3.input")
 
 puts part_a(input)
 puts part_b(input)
